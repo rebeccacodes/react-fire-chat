7e2b1cfd2a7231d5cec529a336bafc5a2f26fd2b
Rebecca Martin <rebecca-martin@live.com>
2018-09-07 17:49:04 -0700

    chat functionality

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,14 @@ import 'materialize-css/dist/css/materialize.min.css';
 import { Route } from 'react-router-dom';
 import Home from './home/home';
 import Chat from './chat/chat';
+import SetName from './set_name';
 
 
 const App = () => (
     <div className="container">
         <Route path="/" exact component={Home} />
         <Route path="/chat" component={Chat} />
+        <Route path="/set-name" component={SetName} />
     </div>
 );
 
